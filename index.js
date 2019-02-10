341834420bab883968590ce07c3ccefdf46f81f6
Andrew S <andrews54757@gmail.com>
2019-02-09 23:21:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-var list = [{
-        "word": "fuck"
-    },
-    {
-        "word": "bitch"
-    },
-    {
-        "word": "nigger"
-
-    },
-    {
-        "word": "dick"
-    },
-    {
-        "word": "cunt"
-
-    }
-]
-
+var list = JSON.parse(require("fs").readFileSync(__dirname + "/swears.json", "utf8"));
+list.sort(function(a, b) {
+    return b.length - a.length;
+})
 
 function escape(text) { // Removes non-letters and duplicated
 
-    var keys = "q w e r t y u i o p a s d f g h j k l z x c v b n m"
+    var keys = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
 
     var k = keys.split(" ")
     var exceptions = { // Exceptions to duplicate removal. Like, fuuuuck turns into fuck, but nigger doesnt get turned to niger
@@ -32,13 +17,14 @@ function escape(text) { // Removes non-letters and duplicated
         l: true,
         s: true
     }
-    var newtext = "";
-    for (var i = 0; i < text.length; i++) {
-        var char = text.charAt(i)
-        if (k.indexOf(char) != -1 && (text.charAt(i - 1) != char || exceptions[char])) newtext += char;
-    }
 
-    return newtext;
+
+
+    return text.split("").map((char) => {
+        return k.indexOf(char) != -1 ? char : " ";
+    }).filter((char, i) => {
+        return text[i - 1] != char || exceptions[char];
+    }).join("");
 }
 
 
@@ -51,14 +37,17 @@ module.exports = function check(text) {
     var co = 0; // Deviations from swear???
     var index = 0; // Where the swear was detected???
     var fo = 0; // more deviation shit
+    var ind = -1;
 
     if (text) text = text.toLowerCase()
     text = escape(text)
-    console.log(text)
+
 
     var fir = []; // List of first characters of swear words
 
-    for (var i = 0; i < list.length; i++) fir.push(list[i].word.charAt(0))
+    for (var i = 0; i < list.length; i++) fir.push(list[i].charAt(0));
+
+    var detected = [];
 
     for (var i = 0; i < text.length; i++) {
         var ch = text.charAt(i)
@@ -68,36 +57,36 @@ module.exports = function check(text) {
 
             if (ch == c) {
                 seq++;
+                co = 0;
                 if (seq >= watch.length) {
-                    return {
+                    detected.push({
                         word: watch,
                         start: index,
                         end: i + 1
-                    };
+                    });
+                    watch = "";
+                    fo = co = 0;
                 }
-                co = 0;
-                continue;
-            }
-
+            } else
             if (co >= chance || fo >= nonchance) {
                 watch = "";
-                i = index + 1;
+                i = index;
                 fo = 0;
                 co = 0;
-                continue;
+            } else {
+                co++;
+                if (co == 1) fo++;
             }
-            co++;
-            if (co == 1) fo++;
-        } else {
-            var ind = fir.indexOf(ch)
+        } else if (i == 0 || text.charAt(i - 1) == " ") {
+            ind = fir.indexOf(ch, ind + 1)
             if (ind != -1) {
                 fo = 0;
                 index = i;
-                watch = list[ind].word
+                watch = list[ind];
                 seq = 1;
             }
         }
 
     }
-    return false
+    return detected;
 }
