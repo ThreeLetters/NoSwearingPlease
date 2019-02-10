d2778d0541854c082512785e5f8c59e6afef152c
Andrew S <andrews54757@gmail.com>
2019-02-09 22:47:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-"use strict"
-const StringBuild = require('StringBuild.js')
-const list = JSON.parse(require('fs').readFileSync('./badwords.json',"utf8"))
+var list = [{
+        "word": "fuck"
+    },
+    {
+        "word": "bitch"
+    },
+    {
+        "word": "nigger"
 
-module.exports = class NoSwearing {
-    constructor() {
-        
-        
+    },
+    {
+        "word": "dick"
+    },
+    {
+        "word": "cunt"
+
     }
-escape(text) {
-    
-    var keys = "q w e r t y u i o p a s d f g h j k l ; ' z x c v b n m"
-    
+]
+
+
+function escape(text) { // Removes non-letters and duplicated
+
+    var keys = "q w e r t y u i o p a s d f g h j k l z x c v b n m"
+
     var k = keys.split(" ")
-    var map = {
+    var exceptions = { // Exceptions to duplicate removal. Like, fuuuuck turns into fuck, but nigger doesnt get turned to niger
         r: true,
         b: true,
         d: true,
@@ -21,93 +32,72 @@ escape(text) {
         l: true,
         s: true
     }
-    var newtext = new StringBuild("");
-    for (var i = 0; i < text.length; i ++) {
-var char = text.charAt(i)
-if (k.indexOf(char) != -1 && (text.charAt(i-1) != char || map[char])) newtext.append(char)
+    var newtext = "";
+    for (var i = 0; i < text.length; i++) {
+        var char = text.charAt(i)
+        if (k.indexOf(char) != -1 && (text.charAt(i - 1) != char || exceptions[char])) newtext += char;
     }
-    
-    return newtext.toString()
+
+    return newtext;
 }
-checkText(text) {
-    
-    
- var a = this.checkProfanity(text)   
-    
-}
-    checkPermutations(text) {
-        
-    }
-    checkProfanity(text) {
-        var lis = [];
-        list.forEach((item)=>{
-          var ind = text.indexOf(item.word);
-            if (ind == -1) return;
-            return {index: ind,word:item.word}
-            
-        })
-        return false
-        
-    }
-    replaceProfanity(text) {
-         var lis = [];
-    
-        list.forEach((item)=>{
-      text = text.replace(new RegExp("/" + item + "/g"))
-            
-        })
-        
-    }
-checkProfanityAdv(text) { 
-    
-    var watch = "";
-    var seq = 0;
-    var chance = 3;
-    var nonchance = 3
-    var co = 0;
-    var index = 0;
-    var fo = 0;
+
+
+module.exports = function check(text) {
+
+    var watch = ""; // Possible swear
+    var seq = 0; // length of detection i guess?
+    var chance = 3; // Max deviations in letter position of swear (eg: fucABCk will be caught, but not fucABCDk)
+    var nonchance = 3 // Dont know. something about max deviations (eg: nAiBgCger will get caught, but not nAiBgCgDer)
+    var co = 0; // Deviations from swear???
+    var index = 0; // Where the swear was detected???
+    var fo = 0; // more deviation shit
+
     if (text) text = text.toLowerCase()
     text = escape(text)
-    var fir = []
-    
-    for (var i = 0; i < list.length; i ++) fir.push(list[i].word.charAt(0))
-    for (var i = 0; i < text.length; i ++) {
+    console.log(text)
+
+    var fir = []; // List of first characters of swear words
+
+    for (var i = 0; i < list.length; i++) fir.push(list[i].word.charAt(0))
+
+    for (var i = 0; i < text.length; i++) {
         var ch = text.charAt(i)
-       if (watch) {
-         var c = watch.charAt(seq)
-         
-         if (seq >= watch.length - 1) {
-             return {word: watch,index:index,ind:ind};
-         }
-           if (ch == c) {
-               seq ++;
-               co = 0;
-               continue;
-           }
-           if (co >= chance || fo >= nonchance) {
-               watch = "";
-               i = ind
-               fo = 0;
-               co = 0;
-               continue;
-           }
-           co ++;
-           if (co == 1) fo ++;
-           continue;
-       } 
-        
-        var ind = fir.indexOf(ch,ind + 1)
-        if (ind != -1) {
-            fo = 0;
-            index = i;
-            watch = list[ind].word
-            seq = 1;
+        if (watch.length) {
+            var c = watch.charAt(seq)
+
+
+            if (ch == c) {
+                seq++;
+                if (seq >= watch.length) {
+                    return {
+                        word: watch,
+                        start: index,
+                        end: i + 1
+                    };
+                }
+                co = 0;
+                continue;
+            }
+
+            if (co >= chance || fo >= nonchance) {
+                watch = "";
+                i = index + 1;
+                fo = 0;
+                co = 0;
+                continue;
+            }
+            co++;
+            if (co == 1) fo++;
+        } else {
+            var ind = fir.indexOf(ch)
+            if (ind != -1) {
+                fo = 0;
+                index = i;
+                watch = list[ind].word
+                seq = 1;
+            }
         }
-    
-        
-        
+
     }
     return false
-}
 }
